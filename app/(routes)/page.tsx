08ebab2b274b01c7853d8b55466a4c123daef9fa
Jhorman Mera <1010159945@u.icesi.edu.co>
2024-06-10 21:41:20 -0500

    Store Updated

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -22,7 +22,7 @@ const HomePage = async () => {
           data={billboard}
         />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-          <ProductList title="Featured Products" items={products} />
+          <ProductList title="Productos de la tienda" items={products} />
         </div>
       </div>
     </Container>
